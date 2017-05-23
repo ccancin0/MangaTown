f1a7d8a644aff9948a4f3a1cd9bcf67ab99e06d9
Carlos <ccancino@memphis.edu>
2017-05-22 21:50:20 -0500

    main content/display almost done, working scroll need work on responsiveness of the main content

--- js/main.js
+++ js/main.js
@@ -6,4 +6,19 @@ $(document).ready(function() {
       $('.container').removeClass('nav-fixed');
     }
   });
+
+  $('.manga-content').jScrollPane();
+
+  var i = 1;
+  setInterval(function() {
+    if (i == 4) {
+      $('.image4').removeClass('current');
+      $('.image1').addClass('current');
+      i = 1;
+    }else {
+      $('.image'+i).removeClass('current');
+      i++;
+      $('.image'+i).addClass('current');
+    }
+  },8000);
 });
